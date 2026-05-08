fabcc127519a72915e9a1de64c46644b18bb7820
dekadjevi <k.dekadjevi@studenti.unipi.it>
2026-05-08 18:39:40 +0200

    update...

--- note_1_diplyr.R
+++ note_1_diplyr.R
@@ -11,9 +11,28 @@ df %>%
 # Arrange verb ("usefull to knoww extrem values in the dataset")
 
 # ascending w.r.t gdp per capita
+
 df %>%
   arrange(gdpPercap)
-#descending
+
+#descending with filter 
+
 df %>%
   filter(year == 2007) %>%
   arrange(desc(gdpPercap))
+
+# using mutate to change a variable
+
+df %>%
+  mutate(pop = pop/1000000) 
+
+df %>%
+  mutate(gdp = gdpPercap*pop) 
+
+# example "country with highest total gdp in year 2007" 
+
+df %>%
+  filter(year == 2007) %>%
+  mutate(gdp = gdpPercap * pop) %>% # this create a column
+  arrange(desc(gdp))
+
