48ed2918a04e9b929671b766f4c1b26955686975
dekadjevi <k.dekadjevi@studenti.unipi.it>
2026-05-08 18:26:55 +0200

    set....

--- Untitled.R
+++ Untitled.R
@@ -3,5 +3,17 @@ gapminder
 library(dplyr)
 df <- gapminder
 
+# FILTER verb , using  this -> pipe operator (%>%) before the verb
+
 df %>%
   filter(year == 1957 , country == "United States")
+
+# Arrange verb ("usefull to knoww extrem values in the dataset")
+
+# ascending w.r.t gdp per capita
+df %>%
+  arrange(gdpPercap)
+#descending
+df %>%
+  filter(year == 2007) %>%
+  arrange(desc(gdpPercap))
