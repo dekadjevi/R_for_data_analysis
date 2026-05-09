7d8f7dc66c07145752864729d82d8b1b0f0ee2ce
dekadjevi <k.dekadjevi@studenti.unipi.it>
2026-05-09 03:51:25 +0200

    new update dplyr

--- note_1_diplyr.R
+++ note_1_diplyr.R
@@ -1,3 +1,5 @@
+install.packages("tidyverse")
+library(gapminder)
 gapminder
 
 library(dplyr)
@@ -24,10 +26,10 @@ df %>%
 # using mutate to change a variable
 
 df %>%
-  mutate(pop = pop/1000000) 
+  mutate(pop = pop/1000000)
 
 df %>%
-  mutate(gdp = gdpPercap*pop) 
+  mutate(gdp = gdpPercap*pop)# this creates a new column gdp 
 
 # example "country with highest total gdp in year 2007" 
 
@@ -36,3 +38,29 @@ df %>%
   mutate(gdp = gdpPercap * pop) %>% # this create a column
   arrange(desc(gdp))
 
+# summarize verb
+
+df%>%
+  filter(year == 2007) %>%
+  summarize(meanlifeExp = mean(lifeExp), totpop = sum(pop)) # average life expe in 2007
+
+# Filter for 1957 then summarize to find median life expectancy and the maximum GDP per capita
+
+df %>%
+  filter(year == 1957) %>%
+  summarize(medianlifeExp = median(lifeExp),max(gdpPercap))
+
+# Grouping verb 
+
+df %>%
+  group_by(year) %>%
+  summarize(meanlifeExp = mean(lifeExp))
+#average lifeExp and total population in 2007 witin each continent
+df %>%
+  filter(year == 2007) %>% 
+  group_by(continent) %>%
+  summarize(avgLifeExp = mean(lifeExp), totpop = sum(pop))
+  
+df %>%
+  group_by(year, continent) %>%
+  summarize(avgLifeExp = mean(lifeExp), totpop = sum(pop))
